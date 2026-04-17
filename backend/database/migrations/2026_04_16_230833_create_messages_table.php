e91ac3a0c3367cf3731dd08f9b7375e6f053cbe8
Yassine-Al <yassinealami2003@gmail.com>
2026-04-17 15:11:15 +0100

    add database structure: Migrations + Models

--- backend/database/migrations/2026_04_16_230833_create_messages_table.php
+++ backend/database/migrations/2026_04_16_230833_create_messages_table.php
@@ -11,11 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+
+            $table->foreignId('sender_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('receiver_id')->constrained('users')->cascadeOnDelete();
+
+            $table->foreignId('annonce_id')->nullable()->constrained()->cascadeOnDelete();
+
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -25,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 };
--- backend/database/migrations/2026_04_16_230835_create_reviews_table.php
+++ backend/database/migrations/2026_04_16_230835_create_reviews_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('annonce_id')->constrained()->cascadeOnDelete();
+
+            $table->integer('rating');
+            $table->text('comment')->nullable();
+
             $table->timestamps();
         });
     }
@@ -25,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
